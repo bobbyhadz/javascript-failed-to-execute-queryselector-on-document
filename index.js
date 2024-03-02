4b56fed0566b4ef7a182170f30bd6ee64fa6d779
bobbyhadz <opmftw@gmail.com>
2024-03-02 14:00:17 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// ✅ Works
+const first = document.querySelector("[id='1box']");
+console.log(first); // 👉️ div#1box
+
+// However, it is better to change the `id` of the element to NOT start with a digit (as shown in the article)
